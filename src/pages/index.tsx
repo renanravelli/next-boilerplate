4ee79e6a8c2cb7dc1c189863ac1c1e483f9eb57e
renanravelli <renanravellialves@gmail.com>
2020-09-26 16:49:11 -0300

    adding support to eslint prettier and husky

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,17 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../../styles/Home.module.css'
 
-export default function Home() {
+export default function Home    ({ title = 'React Avançado!' }) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>{title}</title>
+        <link rel="icon" href="/favicon.ico"/>
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          {title}
         </h1>
 
         <p className={styles.description}>
@@ -57,7 +57,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo}/>
         </a>
       </footer>
     </div>
