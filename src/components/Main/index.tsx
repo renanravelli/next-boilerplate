320b00df0cc7e85ec2dcc64f8425412e8630ea9c
renanravelli <renanravellialves@gmail.com>
2020-09-28 20:49:17 -0300

    feature: adding styled components feature: adding test to style of Main component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,6 +1,17 @@
-const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
-)
-export default Main
+import * as S from './styles'
+
+export default function Home() {
+  return (
+    <S.Wrapper>
+      <S.Logo src="https://d33wubrfki0l68.cloudfront.net/6b19a409f82c8c0c94fff2c927f4dc5bb3291ccd/b93d3/img/logo.svg"
+              alt="Logo do React Avançado"/>
+      <S.Title>React Avançado</S.Title>
+      <S.Description>
+        TypeScript, ReactJS, NextJS e Styled Component
+      </S.Description>
+      <S.Illustration
+        src="https://d33wubrfki0l68.cloudfront.net/cf9dfe86983925690340feb07037ee260f872e34/74dff/img/hero-illustration.svg"
+        alt="Desenvolvendor na frente do computador"/>
+    </S.Wrapper>
+  )
+}
