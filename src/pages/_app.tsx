704d6708fa690972af010e0d558dc4bcd231f055
renanravelli <renanravellialves@gmail.com>
2020-09-30 19:08:40 -0300

    adicionando config pwa

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,7 @@ function App({ Component, pageProps }: AppProps) {
         <title>React Avançado</title>
         <link rel="shortcut icon" href=""/>
         <link rel="apple-touch-icon" href=""/>
+        <link rel="manifest" href="../../public/manifest.json" />
         <meta name="description"
               content="A simple project to work with TypeScript, React,
               NextJS and Styled Components"/>
